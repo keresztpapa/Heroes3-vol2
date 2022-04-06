5d7ab88e68f172c7373da2d7ad5bb527b247b5ca
Ferenc Solyom <ifj.solyom.feco15@gmail.com>
2022-04-06 23:20:32 +0200

    shop

--- src/main/java/com/example/prog1demo/MenuElements/Preparation.java
+++ src/main/java/com/example/prog1demo/MenuElements/Preparation.java
@@ -1,11 +1,18 @@
 package com.example.prog1demo.MenuElements;
 
 import com.example.prog1demo.units.Champions;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -24,9 +31,32 @@ public class Preparation {
         img.setFitWidth(1000);
         img.setFitHeight(800);
 
+        Champions champ = new Champions();
+
         ImageView soldier = new ImageView(new Image("file:pngs/player_units/pike/pike_stand.png"));
         soldier.setX(200);
         soldier.setY(200);
+
+        /*
+        Label solMinLabel = new Label();
+        solMinLabel.setBackground(new Background(new BackgroundFill(Color.rgb(243, 243, 243, 0.9), new CornerRadii(5.0), new Insets(-5.0))));
+        solMinLabel.setTextFill(Color.BLACK);
+        solMinLabel.setMinHeight(50);
+        solMinLabel.setMaxHeight(50);
+        solMinLabel.setMinWidth(50);
+        solMinLabel.setMaxWidth(50);
+        solMinLabel.setLayoutX(170);
+        solMinLabel.setLayoutY(350);
+        */
+        
+        TextField solMinText = new TextField("asd");
+        solMinText.setBackground(new Background(new BackgroundFill(Color.rgb(243, 243, 243, 0.9), new CornerRadii(5.0), new Insets(-5.0))));
+        solMinText.setMinHeight(50);
+        solMinText.setMaxHeight(50);
+        solMinText.setMinWidth(50);
+        solMinText.setMaxWidth(50);
+        solMinText.setLayoutX(170);
+        solMinText.setLayoutY(350);
 
         Button solMin = new Button(" - ");
         solMin.setPrefHeight(50);
@@ -34,11 +64,11 @@ public class Preparation {
         solMin.setLayoutX(100);
         solMin.setLayoutY(350);
 
-        solMin.setOnMouseClicked((event)->{
+        solMin.setOnMouseClicked((event) -> {
             Champions chimp = new Champions();
-            chimp.setGold(chimp.getGold()+50);
-            chimp.setSoldierCount(chimp.getSoldierCount()-1);
-            System.out.println("Chimp gold: "+chimp.getGold());
+            chimp.setGold(chimp.getGold() + 50);
+            chimp.setSoldierCount(chimp.getSoldierCount() - 1);
+            System.out.println("Chimp gold: " + chimp.getGold());
         });
 
         Button solMax = new Button(" + ");
@@ -47,11 +77,11 @@ public class Preparation {
         solMax.setLayoutX(250);
         solMax.setLayoutY(350);
 
-        solMax.setOnMouseClicked((event)->{
+        solMax.setOnMouseClicked((event) -> {
             Champions chimp = new Champions();
-            chimp.setGold(chimp.getGold()-50);
-            chimp.setSoldierCount(chimp.getSoldierCount()+1);
-            System.out.println("Chimp gold: "+chimp.getGold());
+            chimp.setGold(chimp.getGold() - 50);
+            chimp.setSoldierCount(chimp.getSoldierCount() + 1);
+            System.out.println("Chimp gold: " + chimp.getGold());
         });
 
 
@@ -65,11 +95,11 @@ public class Preparation {
         griffMin.setLayoutX(350);
         griffMin.setLayoutY(350);
 
-        griffMin.setOnMouseClicked((event)->{
+        griffMin.setOnMouseClicked((event) -> {
             Champions chimp = new Champions();
-            chimp.setGold(chimp.getGold()+50);
-            chimp.setGriffCount(chimp.getGriffCount()-1);
-            System.out.println("Chimp gold: "+chimp.getGold());
+            chimp.setGold(chimp.getGold() + 50);
+            chimp.setGriffCount(chimp.getGriffCount() - 1);
+            System.out.println("Chimp gold: " + chimp.getGold());
         });
 
         Button griffMax = new Button(" + ");
@@ -78,11 +108,11 @@ public class Preparation {
         griffMax.setLayoutX(500);
         griffMax.setLayoutY(350);
 
-        griffMax.setOnMouseClicked((event)->{
+        griffMax.setOnMouseClicked((event) -> {
             Champions chimp = new Champions();
-            chimp.setGold(chimp.getGold()-50);
-            chimp.setGriffCount(chimp.getGriffCount()+1);
-            System.out.println("Chimp gold: "+chimp.getGold());
+            chimp.setGold(chimp.getGold() - 50);
+            chimp.setGriffCount(chimp.getGriffCount() + 1);
+            System.out.println("Chimp gold: " + chimp.getGold());
         });
 
         ImageView archer = new ImageView(new Image("file:pngs/player_units/archer/archer_stand"));
@@ -95,11 +125,11 @@ public class Preparation {
         archerMin.setLayoutX(600);
         archerMin.setLayoutY(350);
 
-        archerMin.setOnMouseClicked((event)->{
+        archerMin.setOnMouseClicked((event) -> {
             Champions chimp = new Champions();
-            chimp.setGold(chimp.getGold()+50);
-            chimp.setGriffCount(chimp.getGriffCount()-1);
-            System.out.println("Chimp gold: "+chimp.getGold());
+            chimp.setGold(chimp.getGold() + 50);
+            chimp.setGriffCount(chimp.getGriffCount() - 1);
+            System.out.println("Chimp gold: " + chimp.getGold());
         });
 
         Button archerMax = new Button(" + ");
@@ -108,11 +138,11 @@ public class Preparation {
         archerMax.setLayoutX(775);
         archerMax.setLayoutY(350);
 
-        archerMax.setOnMouseClicked((event)->{
+        archerMax.setOnMouseClicked((event) -> {
             Champions chimp = new Champions();
-            chimp.setGold(chimp.getGold()-50);
-            chimp.setGriffCount(chimp.getGriffCount()+1);
-            System.out.println("Chimp gold: "+chimp.getGold());
+            chimp.setGold(chimp.getGold() - 50);
+            chimp.setGriffCount(chimp.getGriffCount() + 1);
+            System.out.println("Chimp gold: " + chimp.getGold());
         });
 
         root.getChildren().add(img);
@@ -125,6 +155,8 @@ public class Preparation {
         root.getChildren().add(griffMax);
         root.getChildren().add(archerMin);
         root.getChildren().add(archerMax);
+     //   root.getChildren().add(solMinLabel);
+        root.getChildren().add(solMinText);
 
         Scene sc = new Scene(root);
         stg.setTitle("Heroes -- Might and Magic 3: Low budget edition :: Shop");
