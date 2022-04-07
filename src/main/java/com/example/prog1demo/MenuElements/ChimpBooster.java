bae92e6b3aca33df19e491f03a3ea1aec8e02b98
Ferenc Solyom <ifj.solyom.feco15@gmail.com>
2022-04-07 22:34:21 +0200

    hero cutomizer in progress, almost done, limitless

--- src/main/java/com/example/prog1demo/MenuElements/ChimpBooster.java
+++ src/main/java/com/example/prog1demo/MenuElements/ChimpBooster.java
@@ -19,6 +19,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class ChimpBooster {
+
     public void boost(Stage stg) throws IOException {
         AnchorPane root = new AnchorPane();
         root.setPrefSize(1000, 800);
@@ -38,18 +39,116 @@ public class ChimpBooster {
         TextField moralText = new TextField();
         TextField luckText = new TextField();
 
+        //attack.setText("Attack:: ");
+        TextField attack = new TextField();
+        TextField defense = new TextField();
+        TextField magic = new TextField();
+        TextField wisdom = new TextField();
+        TextField moral = new TextField();
+        TextField luck = new TextField();
+
+        attText.setText(""+chimp.getAtt());
+        deffText.setText(""+chimp.getDeff());
+        magicText.setText(""+chimp.getMagic_power());
+        wisdomText.setText(""+chimp.getWisdom());
+        moralText.setText(""+chimp.getMoral());
+        luckText.setText(""+chimp.getLuck());
+
         Button attackMinus = new Button(" - ");
+
+        attackMinus.setOnMouseClicked((event)->{
+            chimp.setAtt(chimp.getAtt()-1);
+            chimp.setSkillPoint(chimp.getSkillPoint()+1);
+            attText.setText(""+chimp.getAtt());
+        });
+
         Button attackMax = new Button(" + ");
+
+        attackMax.setOnMouseClicked((event)->{
+            chimp.setAtt(chimp.getAtt()+1);
+            chimp.setSkillPoint(chimp.getSkillPoint()-1);
+            attText.setText(""+chimp.getAtt());
+        });
+
         Button deffMinus = new Button(" - ");
+
+        deffMinus.setOnMouseClicked((event)->{
+            chimp.setDeff(chimp.getDeff()-1);
+            chimp.setSkillPoint(chimp.getSkillPoint()+1);
+            deffText.setText(""+chimp.getDeff());
+        });
+
         Button deffMax = new Button(" + ");
+
+        deffMax.setOnMouseClicked((event)->{
+            chimp.setDeff(chimp.getDeff()+1);
+            chimp.setSkillPoint(chimp.getSkillPoint()-1);
+            deffText.setText(""+chimp.getDeff());
+        });
+
         Button magicMin = new Button(" - ");
+
+        magicMin.setOnMouseClicked((event)->{
+            chimp.setMagic_power(chimp.getMagic_power()-1);
+            chimp.setSkillPoint(chimp.getSkillPoint()+1);
+            magicText.setText(""+chimp.getMagic_power());
+        });
+
         Button magicMax = new Button(" + ");
+
+        magicMax.setOnMouseClicked((event)->{
+            chimp.setMagic_power(chimp.getMagic_power()+1);
+            chimp.setSkillPoint(chimp.getSkillPoint()-1);
+            magicText.setText(""+chimp.getMagic_power());
+        });
+
         Button wisdomMin = new Button(" - ");
+
+        wisdomMin.setOnMouseClicked((event)->{
+            chimp.setWisdom(chimp.getWisdom()-1);
+            chimp.setSkillPoint(chimp.getSkillPoint()+1);
+            wisdomText.setText(""+chimp.getWisdom());
+        });
+
         Button wisdomMax = new Button(" + ");
+
+        wisdomMax.setOnMouseClicked((event)->{
+            chimp.setWisdom(chimp.getWisdom()+1);
+            chimp.setSkillPoint(chimp.getSkillPoint()-1);
+            wisdomText.setText(""+chimp.getWisdom());
+        });
+
         Button moralMin = new Button(" - ");
+
+        moralMin.setOnMouseClicked((event)->{
+            chimp.setMoral(chimp.getMoral()-1);
+            chimp.setSkillPoint(chimp.getSkillPoint()+1);
+            moralText.setText(""+chimp.getMoral());
+        });
+
         Button moralMax = new Button(" + ");
+
+        moralMax.setOnMouseClicked((event)->{
+            chimp.setMoral(chimp.getMoral()+1);
+            chimp.setSkillPoint(chimp.getSkillPoint()-1);
+            moralText.setText(""+chimp.getMoral());
+        });
+
         Button luckMin = new Button(" - ");
+
+        luckMin.setOnMouseClicked((event)->{
+            chimp.setLuck(chimp.getLuck()-1);
+            chimp.setSkillPoint(chimp.getSkillPoint()+1);
+            luckText.setText(""+chimp.getLuck());
+        });
+
         Button luckyMax = new Button(" + ");
+
+        luckyMax.setOnMouseClicked((event)->{
+            chimp.setLuck(chimp.getLuck()+1);
+            chimp.setSkillPoint(chimp.getSkillPoint()-1);
+            luckText.setText(""+chimp.getLuck());
+        });
 
         serializeButton(attackMinus, 300, 150);
         serializeButton(attackMax, 500, 150);
@@ -64,93 +163,35 @@ public class ChimpBooster {
         serializeButton(luckMin, 300, 650);
         serializeButton(luckyMax, 500, 650);
 
-        attText.setText("Att"+chimp.getAtt());
-        attText.setBackground(new Background(new BackgroundFill(Color.rgb(243, 243, 243, 0.9), new CornerRadii(5.0), new Insets(-5.0))));
-        attText.setMinHeight(50);
-        attText.setMaxHeight(50);
-        attText.setMinWidth(50);
-        attText.setMaxWidth(50);
-        attText.setLayoutX(400);
-        attText.setLayoutY(150);
+        setTextBox(attText,400,150);
+        setTextBox(deffText,400,250);
+        setTextBox(magicText,400,350);
+        setTextBox(wisdomText,400,450);
+        setTextBox(moralText,400,550);
+        setTextBox(luckText,400,650);
 
-        deffText.setText("deff"+chimp.getDeff());
-        deffText.setBackground(new Background(new BackgroundFill(Color.rgb(243, 243, 243, 0.9), new CornerRadii(5.0), new Insets(-5.0))));
-        deffText.setMinHeight(50);
-        deffText.setMaxHeight(50);
-        deffText.setMinWidth(50);
-        deffText.setMaxWidth(50);
-        deffText.setLayoutX(400);
-        deffText.setLayoutY(250);
+        attack.setText("Attack:: ");
+        setTextBox(attack, 150 ,150);
 
-        magicText.setText("mag"+chimp.getMagic_power());
-        magicText.setBackground(new Background(new BackgroundFill(Color.rgb(243, 243, 243, 0.9), new CornerRadii(5.0), new Insets(-5.0))));
-        magicText.setMinHeight(50);
-        magicText.setMaxHeight(50);
-        magicText.setMinWidth(50);
-        magicText.setMaxWidth(50);
-        magicText.setLayoutX(400);
-        magicText.setLayoutY(350);
+        defense.setText("Defense:: ");
+        setTextBox(defense,150,250);
 
-        wisdomText.setText("wis"+chimp.getWisdom());
-        wisdomText.setBackground(new Background(new BackgroundFill(Color.rgb(243, 243, 243, 0.9), new CornerRadii(5.0), new Insets(-5.0))));
-        wisdomText.setMinHeight(50);
-        wisdomText.setMaxHeight(50);
-        wisdomText.setMinWidth(50);
-        wisdomText.setMaxWidth(50);
-        wisdomText.setLayoutX(400);
-        wisdomText.setLayoutY(450);
+        magic.setText("Magic:: ");
+        setTextBox(magic,150,350);
 
-        moralText.setText("mor"+chimp.getMoral());
-        moralText.setBackground(new Background(new BackgroundFill(Color.rgb(243, 243, 243, 0.9), new CornerRadii(5.0), new Insets(-5.0))));
-        moralText.setMinHeight(50);
-        moralText.setMaxHeight(50);
-        moralText.setMinWidth(50);
-        moralText.setMaxWidth(50);
-        moralText.setLayoutX(400);
-        moralText.setLayoutY(550);
+        wisdom.setText("Wisdom:: ");
+        setTextBox(wisdom,150,450);
 
-        luckText.setText("luc"+chimp.getLuck());
-        luckText.setBackground(new Background(new BackgroundFill(Color.rgb(243, 243, 243, 0.9), new CornerRadii(5.0), new Insets(-5.0))));
-        luckText.setMinHeight(50);
-        luckText.setMaxHeight(50);
-        luckText.setMinWidth(50);
-        luckText.setMaxWidth(50);
-        luckText.setLayoutX(400);
-        luckText.setLayoutY(650);
+        moral.setText("Moral:: ");
+        setTextBox(moral,150,550);
 
-/*
-        ImageView griff = new ImageView(new Image("file:pngs/player_units/griff/griff_stand.png"));
-        griff.setX(400);
-        griff.setY(200);
+        luck.setText("Luck:: ");
+        setTextBox(luck,150,650);
 
-        griffText = new TextField(":: "+champ.getGriffCount());
-        griffText.setBackground(new Background(new BackgroundFill(Color.rgb(243, 243, 243, 0.9), new CornerRadii(5.0), new Insets(-5.0))));
-        griffText.setMinHeight(50);
-        griffText.setMaxHeight(50);
-        griffText.setMinWidth(50);
-        griffText.setMaxWidth(50);
-        griffText.setLayoutX(425);
-        griffText.setLayoutY(350);*/
-
-
-        root.getChildren().add(attackMinus);
-        root.getChildren().add(attackMax);
-        root.getChildren().add(deffMinus);
-        root.getChildren().add(deffMax);
-        root.getChildren().add(magicMin);
-        root.getChildren().add(magicMax);
-        root.getChildren().add(wisdomMin);
-        root.getChildren().add(wisdomMax);
-        root.getChildren().add(moralMin);
-        root.getChildren().add(moralMax);
-        root.getChildren().add(luckMin);
-        root.getChildren().add(luckyMax);
-        root.getChildren().add(attText);
-        root.getChildren().add(deffText);
-        root.getChildren().add(magicText);
-        root.getChildren().add(wisdomText);
-        root.getChildren().add(moralText);
-        root.getChildren().add(luckText);
+        root.getChildren().addAll(img, attackMinus, attackMax, deffMinus, deffMax,magicMin
+        , magicMax, wisdomMin, wisdomMax, moralMin, moralMax, luckMin, luckyMax, attText,
+                deffText, magicText, wisdomText, moralText, luckText, attack, defense,
+                magic, wisdom, moral, luck);
 
         Scene sc = new Scene(root);
         stg.setTitle("Heroes -- Might and Magic 3: Low budget edition :: Skill point defection");
@@ -158,6 +199,15 @@ public class ChimpBooster {
         stg.show();
     }
 
+    public void setTextBox(TextField str, int x, int y){
+        str.setBackground(new Background(new BackgroundFill(Color.rgb(243, 243, 243, 0.9), new CornerRadii(5.0), new Insets(-5.0))));
+        str.setMinHeight(50);
+        str.setMaxHeight(50);
+        str.setMinWidth(50);
+        str.setMaxWidth(75);
+        str.setLayoutX(x);
+        str.setLayoutY(y);
+    }
 
     public void serializeButton(Button btn, int x_cord, int y_cord){
         btn.setPrefHeight(50);
